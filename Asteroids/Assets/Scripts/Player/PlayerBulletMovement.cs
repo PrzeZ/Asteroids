c797f9b4c4f5b079edf74cbc8bc278082bfdc66b
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2022-03-02 17:50:39 +0100

    Added bullet move

---
+++ Asteroids/Assets/Scripts/Player/PlayerBulletMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float bulletSpeed = 10f;
+
+   private void OnBecameVisible()
+    {
+        rb.AddRelativeForce(new Vector2(0, bulletSpeed));
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+}
