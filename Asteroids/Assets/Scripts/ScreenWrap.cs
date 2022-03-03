6632893941c66d96e39804182582ab134aa9b402
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2022-03-03 14:43:09 +0100

    Added screen wrap

---
+++ Asteroids/Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ScreenWrap : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    private bool isWrappingX = false;
+    private bool isWrappingY = false;
+
+    private void OnBecameInvisible()
+    {
+        if (isWrappingX && isWrappingY)
+        {
+            return;
+        }
+
+        Vector3 viewportPosition = cam.WorldToViewportPoint(transform.position);
+        Vector3 newPosition = transform.position;
+
+        if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+
+            isWrappingX = true;
+        }
+
+        if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private void OnBecameVisible()
+    {
+        isWrappingX = false;
+        isWrappingY = false;
+    }
+}
