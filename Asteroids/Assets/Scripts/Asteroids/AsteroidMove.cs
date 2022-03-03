590b9fd64a2a922b53302153d26b99acab07501f
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2022-03-03 15:49:38 +0100

    Added moving asteroids

---
+++ Asteroids/Assets/Scripts/Asteroids/AsteroidMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AsteroidMove : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private float size = 1f;
+    [SerializeField] private float minSize = 0.35f;
+    [SerializeField] private float maxSize = 1.65f;
+    [SerializeField] private float movementSpeed = 1f;
+
+    private void OnEnable()
+    {
+        transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360f);
+
+        // Set the scale and mass of the asteroid based on the assigned size so
+        // the physics is more realistic
+        transform.localScale = Vector3.one * size;
+        rigidbody.mass = size;
+        SetTrajectory(new Vector2(Random.value, Random.value));
+
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        rigidbody.AddForce(direction * movementSpeed);
+    }
+}
